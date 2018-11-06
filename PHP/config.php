e7e4ffa2e5eabd466102078db3df42691938e7bf
ariqfmutyara <ariqmutyara@gmail.com>
2018-11-06 17:50:03 +0700

    Updated

--- PHP/config.php
+++ PHP/config.php
@@ -6,6 +6,6 @@ $dbuser = "root";
 $dbpassword = "";
 
 $koneksi = mysqli_connect ($dbhost,$dbuser,$dbpassword) or die ("koneksi ke server error!");
-mysqli_select_db($dbname,$koneksi) or die ("koneksi ke database error!");
+mysqli_select_db($koneksi,$dbname) or die ("koneksi ke database error!");
 
 ?>
