580cb5729c73c8e6a0e50043253ea214c5f0cdb7
agneshata <agneshata68@gmail.com>
2018-11-13 11:49:22 +0700

    sudah memakai CI ea

---
+++ application/views/explore.php
@@ -0,0 +1,4 @@
+<div class=square1>
+			<div class="square2">
+				<div class="explore"><a href="#Explore." style="text-decoration: none;font-family: montserrat;color: white;">Explore.</a></div>
+			</div>
---
+++ application/views/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>VOTEART</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+		    <link rel="stylesheet" href="<?php echo base_url()?>assets/CSS/style_home_basic.css">
+		    <ul>
+		        <li><a href="<?php echo base_url()?>register">REGISTER</a></li>
+		        <li><a href="<?php echo base_url()?>login">LOG IN</a></li>
+		        <li class="dropdown">
+		            <a href="javascript:void(0)" class="dropbtn">CATEGORIES</a>
+		            <div class="dropdown-content">
+		                <a href="#outdoor">Food</a>
+		                <a href="#indoor">Landscape</a>
+		                <a href="#animal">Fashion</a>
+		                <a href="#pop art">Art</a>
+		                <a href="#pop art">Architecture</a>
+		                <a href="#pop art">Typography</a>
+
+		        <li><a href="#news">HOME</a></li>
+		            <li>
+		            <div class="search-container">
+		                <form action="#searchbar">
+		                    <input style="border-radius:5px; border-style: none;height: 20px;margin-left: 10px"type="text" placeholder="Search more Art. . ." name="search">
+		                        <button type="submit" style="border-radius:5px; border-style: none;height: 20px;" >Go!</button>
+		                </form>
+		            </div>  
+		        </li>
+		            </div>
+		        </li>
+		    </ul>
+		</div>
+	</div>
+</body>
+</html>
