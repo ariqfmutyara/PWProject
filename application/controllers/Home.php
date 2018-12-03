81eea0ad89e46eea6e334d5ed73726ea7008b241
agneshata <agneshata68@gmail.com>
2018-12-03 11:47:52 +0700

    commit baru

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,8 +6,7 @@ class Home extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('header');
-		$this->load->view('navbar');
-		$this->load->view('explore');
+		
 	}
 }
 ?>
--- application/views/header.php
+++ application/views/header.php
@@ -1,43 +1,267 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>VOTEART</title>
+
 	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+
+	<title>Vote Art</title>
+
+    <!-- (gunanya untuk buat jarak antar foto)
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    ================================================== -->
+
+    <!-- Animate CSS (masih belum tau gunanya buat apa)
+        <link rel="stylesheet" href="css/animate.min.css">
+   ================================================== -->
+	
+
+    <!-- Font Icons CSS 
+        (gunanya buat twiter facebook etc)
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        (gunanya buat strip 3 diatas)
+	<link rel="stylesheet" href="css/ionicons.min.css">
+   ================================================== -->
+   
+
+	<!-- Main CSS
+   ================================================== -->
+   <link rel="stylesheet" href="<?php echo base_url();?>assets/CSS/style.css">
+
+    <!-- Google web font (masih belum tau fungsinya di font yg mana)
+         <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
+   ================================================== -->	
+   <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
+	
 </head>
 <body>
-	<div class="container">
-		<div class="header">
-		    <link rel="stylesheet" href="<?php echo base_url();?>assets/CSS/style_home_basic.css">
-		    <ul>
-		        <li><a href="<?php echo base_url()?>register">REGISTER</a></li>
-				<li class = "dropdown">
-					<a href="<?php echo base_url()?>login" href="javascript:void(0)" class="dropbtn" >LOG IN</a>
-					<div class="dropdown-content">
-					<a href="#outdoor">as Voterss</a>
-		            <a href="#indoor">as Artist</a>
-						
-		        <li class="dropdown">
-		            <a href="javascript:void(0)" class="dropbtn">CATEGORIES</a>
-		            <div class="dropdown-content">
-		                <a href="#outdoor">Food</a>
-		                <a href="#indoor">Landscape</a>
-		                <a href="#animal">Fashion</a>
-		                <a href="#pop art">Art</a>
-		                <a href="#pop art">Architecture</a>
-		                <a href="#pop art">Typography</a>
 
-		        <li><a href="#news">HOME</a></li>
-		            <li>
-		            <div class="search-container">
-		                <form action="#searchbar">
-		                    <input style="border-radius:5px; border-style: none;height: 20px;margin-left: 10px"type="text" placeholder="Search more Art. . ." name="search">
-		                        <button type="submit" style="border-radius:5px; border-style: none;height: 20px;" >Go!</button>
-		                </form>
-		            </div>  
-		        </li>
-		            </div>
-		        </li>
-		    </ul>
+<!-- Preloader section (masih belum tau fungsinya dimana)
+    <div class="preloader">
+
+	<div class="sk-spinner sk-spinner-pulse"></div>
+
+</div>
+================================================== -->
+<!-- masihh di cobaaa
+<nav id = "menu">
+    <ul>
+        <div id="heading">
+            <a href="#"></a>
+            
+        </div>
+    </ul>
+</nav>
+====== -->
+<!--
+<div class="preloader">
+
+	<div class="sk-spinner sk-spinner-pulse"></div>
+
+</div>
+-->
+
+<!-- Navigation section (ini udh ga perlu, soalnya kan kita gapake strip 3)
+================================================== -->
+<div class="nav-container">
+      <nav class="nav-inner transparent">
+   
+        <div class="navbar">
+          <div class="container">
+             <div class="row">
+ 
+               <div class="brand">
+                 <a href="index.html" class="wow fadeIn" data-wow-delay="1.4s">VoteArt</a>
+               </div>
+
+                 <div class="navicon">
+                <div class="menu-container">
+                  <div>
+                    <div>
+                      <ul>
+                        <li><a href="index.html">Log In</a></li>
+                        </ul>
+                      </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+            </nav>
+            </div>
+</body>
+
+   
+    
+  
+<!-- Header section (ini mah bakal di ganti sama slide poto)
+================================================== -->
+<section id="header" class="header-one">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-3 col-md-6 col-sm-offset-2 col-sm-8">
+          <div class="header-thumb">
+            <div id="slider" class="wow fadeIn">
+              <figure>
+			  <a href="#"><img src="<?php echo base_url();?>assets/foto/blog-img9.jpg"></a>
+			  <a href="#"><img src="<?php echo base_url();?>assets/foto/blog-img8.jpg"></a>
+			  <a href="#"><img src="<?php echo base_url();?>assets/foto/blog-img7.jpg"></a>
+			  <a href="#"><img src="<?php echo base_url();?>assets/foto/blog-img9.jpg"></a>
+			  <a href="#"><img src="<?php echo base_url();?>assets/foto/blog-img6.jpg"></a>
+              </figure>
+            </div>
+              <h1 class="wow fadeIn" data-wow-delay="1.7s">Find Your Inspiration</h1>
+              <h3 class="wow fadeInUp" data-wow-delay="2.0s">Appreciate the Creation</h3>
+          </div>
+			</div>
+
+		</div>
+	</div>		
+
+
+
+<!-- Portfolio section (masih belum ngerti pengaturan data ttg data filternya)
+================================================== -->
+<section id="portfolio">
+      <div class="container">
+         <div class="row">
+   
+            <div class="col-md-12 col-sm-12">
+               
+                  <!-- iso section -->
+                  <div class="iso-section wow fadeInUp" data-wow-delay="1.6s">
+   
+                     <ul class="filter-wrapper clearfix">
+                              <li><a href="#" data-filter="*" class="selected opc-main-bg">All</a></li>
+                              <li><a href="#" class="opc-main-bg" data-filter=".graphic">Graphic</a></li>
+                              <li><a href="#" class="opc-main-bg" data-filter=".template">Web template</a></li>
+                              <li><a href="#" class="opc-main-bg" data-filter=".photoshop">Photoshop</a></li>
+                           <li><a href="#" class="opc-main-bg" data-filter=".branding">Branding</a></li>
+                           </ul>
+   
+                           <!-- iso box section -->
+                           <div class="iso-box-section wow fadeInUp" data-wow-delay="1s">
+                              <div class="iso-box-wrapper col4-iso-box">
+   
+                                 <div class="iso-box photoshop branding col-md-4 col-sm-6">
+                                    <div class="portfolio-thumb">
+                                       <img src="assets/foto/portfolio-img1.jpg" class="img-responsive" alt="Portfolio">
+                                          <div class="portfolio-overlay">
+                                             <div class="portfolio-item">
+                                                   <a href="single-project.html"><i class="fa fa-link"></i></a>
+                                                   <h2>Project One</h2>
+                                                </div>
+                                          </div>
+                                    </div>
+                                 </div>
+   
+                                 <div class="iso-box graphic template col-md-4 col-sm-6">
+                                    <div class="portfolio-thumb">
+                                       <img src="assets/foto/portfolio-img2.jpg" class="img-responsive" alt="Portfolio">
+                                          <div class="portfolio-overlay">
+                                             <div class="portfolio-item">
+                                                   <a href="single-project.html"><i class="fa fa-link"></i></a>
+                                                   <h2>Project Two</h2>
+                                                </div>
+                                          </div>
+                                    </div>
+                                 </div>
+   
+                                 <div class="iso-box template graphic col-md-4 col-sm-6">
+                                    <div class="portfolio-thumb">
+                                       <img src="assets/foto/portfolio-img3.jpg" class="img-responsive" alt="Portfolio">
+                                          <div class="portfolio-overlay">
+                                             <div class="portfolio-item">
+                                                   <a href="single-project.html"><i class="fa fa-link"></i></a>
+                                                   <h2>Project Three</h2>
+                                                </div>
+                                          </div>
+                                    </div>
+                                 </div>
+   
+                                 <div class="iso-box graphic template col-md-4 col-sm-6">
+                                    <div class="portfolio-thumb">
+                                       <img src="assets/foto/portfolio-img4.jpg" class="img-responsive" alt="Portfolio">
+                                          <div class="portfolio-overlay">
+                                             <div class="portfolio-item">
+                                                   <a href="single-project.html"><i class="fa fa-link"></i></a>
+                                                   <h2>Project Four</h2>
+                                                </div>
+                                          </div>
+                                    </div>
+                                 </div>
+   
+                                 <div class="iso-box photoshop branding col-md-4 col-sm-6">
+                                    <div class="portfolio-thumb">
+                                       <img src="assets/foto/portfolio-img5.jpg" class="img-responsive" alt="Portfolio">
+                                          <div class="portfolio-overlay">
+                                             <div class="portfolio-item">
+                                                   <a href="single-project.html"><i class="fa fa-link"></i></a>
+                                                   <h2>Project Five</h2>
+                                                </div>
+                                          </div>
+                                    </div>
+                                 </div>
+   
+                                 <div class="iso-box graphic branding col-md-4 col-sm-6">
+                                    <div class="portfolio-thumb">
+                                       <img src="assets/foto/portfolio-img6.jpg" class="img-responsive" alt="Portfolio">
+                                          <div class="portfolio-overlay">
+                                             <div class="portfolio-item">
+                                                   <a href="single-project.html"><i class="fa fa-link"></i></a>
+                                                   <h2>Project Six</h2>
+                                                </div>
+                                          </div>
+                                    </div>
+                                 </div>
+   
+                               </div>
+                           </div>
+   
+                  </div>
+   
+            </div>
+   
+         </div>
+      </div>
+   </section>
+   </section>
+
+
+<!-- Footer section
+
+<footer> (gausah ada)
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+				<p class="wow fadeInUp"  data-wow-delay="0.3s">Copyright © 2016 Your Company Name - Designed by Tooplate</p>
+				<ul class="social-icon wow fadeInUp"  data-wow-delay="0.6s">
+					<li><a href="#" class="fa fa-facebook"></a></li>
+					<li><a href="#" class="fa fa-twitter"></a></li>
+					<li><a href="#" class="fa fa-dribbble"></a></li>
+					<li><a href="#" class="fa fa-behance"></a></li>
+					<li><a href="#" class="fa fa-google-plus"></a></li>
+				</ul>
+			</div>
+			
 		</div>
 	</div>
+</footer>
+================================================== -->
+
+
+<!-- Javascript 
+================================================== -->
+<script src="js/jquery.js"></script>
+<script src="js/isotope.js"></script>
+<script src="js/imagesloaded.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
