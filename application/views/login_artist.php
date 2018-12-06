60e0f68ddcd4ed520a3522c0f96a659c509aeeb8
agneshata <agneshata68@gmail.com>
2018-12-06 18:40:14 +0700

    benerin sambungannya

--- application/views/login_artist.php
+++ application/views/login_artist.php
@@ -35,18 +35,24 @@
                         <label for="PASSWORD" style="color: white; font-family: montserrat; font-size: 20px"><b>Password</b></label>
                         <input type="password" placeholder="Enter Password" name="pw_user" required>
                         
-                        <button type="submit" name="SUBMIT" value="Sign In" style="border-radius: 10px;margin-left: 150px">Login</button>
+                        <form method ="get" action="<?php echo base_url("/Login")?>">
+                            <button type="submit" value="Sign In" style="border-radius: 10px;margin-left: 150px">Login</button>
+                        </form>
                         <label style="color: black; font-family: montserrat; margin-left: 20px; display: inline-block;">
                             <input type="checkbox" checked="checked" name="remember">Remember me
                         </label>
                     </div>
                     <div class="container">
-                        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn" style="border-radius: 10px; margin-left: 150px;" href="Header.php">Cancel</button>
-                            <br>
+                    <form method ="get" action="<?php echo base_url("/Home")?>">
+                        <button type="submit" class="cancelbtn" style="border-radius: 10px; margin-left: 150px;">Cancel</button>
+                        
+                    </form>
+                    <br>
                                 <span class="psw" style="font-family: montserrat; text-decoration: none; color:;margin-left: 250px;">Forgot <a href="#"  style="font-family: montserrat; text-decoration: none;color :black">password?</a></span>
                             </br>
                      <div class="container">
-                        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn" style="border-radius: 0px; margin-top:80px;margin-left: 150px;color: #252525; background-color: #ececec ">REGISTER</button>
+                     <form method ="get" action="<?php echo base_url("/register_voter")?>">
+                        <button type="submit" class="cancelbtn" style="border-radius: 0px; margin-top:80px;margin-left: 150px;color: #252525; background-color: #ececec ">REGISTER</button>
                     </div>
                   </form>
             </div>
