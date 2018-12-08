d5500a45d4eb74a9881d7ba03ec63532d9803b38
luthfiansyh <muhammadluthfiansyah15@gmail.com>
2018-12-08 12:19:42 +0700

    tampilan post artist

---
+++ application/views/tampilan_post_artist.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VOTEART</title>
+	<link rel="stylesheet" type="text/css" href="assets/CSS/post-style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div>
+		<button class="button">
+			<a href="D:/post.html" id="backward" class="fa fa-backward"></a>
+		</button>
+	</div>
+	<div class="post-photo">
+		<div class="konten-grid1">
+			<div class="konten-photo" style="">
+				<div class="image">
+					<img src="D:\ooo\photo5.png">
+				</div>
+			</div>
+			<div class="konten-photo" style="width: 260px;height: 500px;margin-top: 10px;">
+				<div class="konten-isi">
+					<div class="user-acc">
+						<h1>Nama User</h1>
+						<a href="#" id="username" style="opacity: 1;">@Username</a>
+						<img style="margin-top:10px;margin-left: 50px;border-radius: 50px 50px 50px 50px;" src="D:\ooo\photo5.png">
+
+
+					</div>
+					<div class="caption">
+						<textarea readonly>
+							hay tayo Dia bis kecil ramah melaju melambat tayo Banana Banana Hey whatsapp ma friend hellow anyway busway hay tayo Dia bis kecil ramah melaju melambat tayo Banana Banana Hey whatsapp ma friend hellow anyway buswayhay tayo Dia bis kecil ramah melaju melambat tayo Banana Banana Hey whatsapp ma friend hellow anyway busway 
+						</textarea>
+							<div class="konten-grid2">
+								<div class="like-button" >
+									<i class="fa fa-thumbs-up" id="thumb1" style="">
+										<p>Like</p>
+									</i>
+								</div>
+								<div class="like-button" >
+									<i class="fa fa-trash" id="hapus">
+										<p>Delete</p>
+										
+										
+									</i>
+								</div>
+								<div class="like-button" >
+									<i class="fa fa-edit" id="edit">
+										<p>Edit</p>
+										
+									</i>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+
+
+</body>
+</html>
